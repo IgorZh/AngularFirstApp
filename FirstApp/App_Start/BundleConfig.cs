e20a4fd3b8dc3c66c80df8bcd880818feaf0d17c
andrew <andrey.samsonov@api.nnov.ru>
2013-09-27 14:35:20 +0400

    #ts-1 fixed

--- FirstApp/App_Start/BundleConfig.cs
+++ FirstApp/App_Start/BundleConfig.cs
@@ -11,8 +11,7 @@ namespace FirstApp
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/angular.js",
-                        "~/Scripts/angular-resource.js"
-                        ));
+                        "~/Scripts/angular-resource.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
